6182de24dfe3195e75f353b1f3331970f5cdf674
dwhite54 <david.white.679@gmail.com>
2019-09-07 14:03:07 -0600

    Add artifact (jar file) to IDE config Fix processbuilder calls, verified working on CSU CS dept machines now Beginning to switch flags around to be more modular and explicit

--- src/com/cs555/a1/Main.java
+++ src/com/cs555/a1/Main.java
@@ -3,6 +3,7 @@ package com.cs555.a1;
 import org.w3c.dom.ranges.RangeException;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
@@ -40,8 +41,15 @@ public class Main {
                         if (chunkMachines.length == 0)
                             throw new IllegalArgumentException("Error parsing chunk machine list");
                         break;
-                    case "--noclient":
+                    case "--is-client":
                         isClient = false;
+                        break;
+                    case "--is-controller":
+                        isClient = false;
+                        break;
+                    case "--is-chunkserver":
+                        isClient = false;
+                        break;
                 }
             } catch (RangeException | IllegalArgumentException e) {  //catch range and parsing errors
                 System.out.println("Error parsing arguments");
@@ -55,40 +63,70 @@ public class Main {
             printUsage();
         } // ignore other cases, since those should be managed programmatically
 
-        String mainPath = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        if (isClient)
+            spawnProcesses(controllerPort, chunkPort, controllerMachine, chunkMachines);
+        else if (controllerPort != 0) { // we now know that this was a child process, configured to start a controller proc
+
+        } else if (chunkPort != 0) {
+
+        }
+
+
+        Close();
+        System.exit(0);
+    }
+
+    private static void spawnProcesses(int controllerPort, int chunkPort, String controllerMachine, String[] chunkMachines) {
+        String jarPath = String.format("%s/cs555-a1.jar", System.getProperty("user.dir"));
         Processes = new Process[chunkMachines.length+1];
 
         for (int i = 0; i < chunkMachines.length; i++) {
             String cmdArgs = String.format("--noclient --chunk-port %s", chunkPort);
-            StartProcess(chunkMachines[i], mainPath, cmdArgs, i);
+            StartProcess(chunkMachines[i], jarPath, cmdArgs, i);
         }
 
         String cmdArgs = String.format("--noclient --controller-port %s", controllerPort);
-        StartProcess(controllerMachine, mainPath, cmdArgs, chunkMachines.length);
+        StartProcess(controllerMachine, jarPath, cmdArgs, chunkMachines.length);
 
-        //TODO check that each process is started?
-        //TODO create client object to talk to Controller and Chunk Servers
-        //TODO check that ssh process logic is working
-        //TODO fill out remaining actual assignment logic
+        for (Process p : Processes) {
+            if (!p.isAlive() && p.exitValue() == 255) {
+                System.out.printf("Process %s closed with -1 exit value%n", p.toString());
+                String input = "";
+                String errors = "";
+                try {
+                    input = new String(p.getInputStream().readAllBytes());
+                    errors = new String(p.getErrorStream().readAllBytes());
+                } catch (IOException e) {
+                    System.out.println("Error occurred reading input and error streams:" + e.getMessage());
+                    Close();
+                    System.exit(-1);
+                }
+                System.out.printf("InputStream:%s%n", input);
+                System.out.printf("ErrorStream:%s%n", errors);
+                Close();
+                System.exit(-1);
+            }
+        }
     }
 
     private static void printUsage() {
         System.out.println("Options:");
+        System.out.println("\t--mode: determines whether to run this process as controller, chunk server, " +
+                "or client, starting controller and chunk servers if not detected");
         System.out.println("\t--controller-port: port the controller will communicate with");
         System.out.println("\t--controller-machine: machine the controller will run on");
         System.out.println("\t--chunk-port: port the chunk servers will communicate with");
-        System.out.println("\t--chunk-machines: (comma-delimited) list of machines to start chunk servers on");
-        //hidden arg: --noclient, which just starts chunk server or controller (otherwise running as client)
     }
 
     private static void StartProcess(String machine, String path, String args, int pIdx) {
-        String cmd = String.format("ssh %s -t \"java -jar %s %s\"",
-                machine, path, args);
+        String javaCmd = String.format("java -jar %s %s", path, args);
+        List<String> cmd = List.of("ssh", machine, "-t", javaCmd);
         ProcessBuilder b = new ProcessBuilder(cmd);
         try {
             Processes[pIdx] = b.start();
+            System.out.printf("Started process %s using command %s%n", Processes[pIdx].toString(), cmd.toString());
         } catch (IOException e) {
-            System.out.println("Error starting chunk process on machine " + machine);
+            System.out.printf("Error starting chunk process on machine %s%n", machine);
             System.out.println(e.getMessage());
             Close();
             System.exit(-1);
@@ -99,8 +137,10 @@ public class Main {
         System.out.println("Closing...");
         if (Processes != null) {
             for (Process p : Processes) {
-                if (p != null)
+                if (p != null) {
+                    System.out.println("Found running process " + p.toString() + " now closing.");
                     p.destroy();
+                }
             }
         }
     }
